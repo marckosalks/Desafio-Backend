4ee8c478123b78f5180e34098ad7bf876a65f15b
marckosalks <m4rcos.sales@gmail.com>
2026-04-30 04:01:00 -0300

    feat: ajustando erro na hora de cadastrar os logs

--- cep-consulta-service/src/main/java/br/com/f1rst/cep_consulta_service/service/CepService.java
+++ cep-consulta-service/src/main/java/br/com/f1rst/cep_consulta_service/service/CepService.java
@@ -1,28 +1,38 @@
 package br.com.f1rst.cep_consulta_service.service;
 
 import br.com.f1rst.cep_consulta_service.client.ViaCepClient;
-import br.com.f1rst.cep_consulta_service.dto.CepDto;
-import br.com.f1rst.cep_consulta_service.dto.CepResponseDto;
-import br.com.f1rst.cep_consulta_service.dto.ViaCepResponse;
+import br.com.f1rst.cep_consulta_service.dto.*;
+import br.com.f1rst.cep_consulta_service.entity.CepLog;
 import br.com.f1rst.cep_consulta_service.exception.CepNotFoundException;
+import br.com.f1rst.cep_consulta_service.repository.CepLogRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CepService {
 
     private final ViaCepClient viaCepClient;
+    private final CepLogRepository repository;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public CepService(ViaCepClient viaCepClient) {
+    public CepService(ViaCepClient viaCepClient,
+                      CepLogRepository repository) {
         this.viaCepClient = viaCepClient;
+        this.repository = repository;
     }
 
     public CepResponseDto buscarCep(CepDto cepDto) throws CepNotFoundException {
 
         ViaCepResponse response = viaCepClient.buscarCep(cepDto.getCep());
 
-        if(response.getErro() != null && response.getErro()){
+        if (response.getErro() != null && response.getErro()) {
             throw new CepNotFoundException("Cep Não encontrado!");
         }
+
+        // 🔥 salvar log
+        salvarLog(cepDto.getCep(), response);
 
         return CepResponseDto.builder()
                 .cep(response.getCep())
@@ -33,9 +43,21 @@ public class CepService {
                 .build();
     }
 
-    public String guardaLogs(String data) {
-        return data;
+    private void salvarLog(String cep, ViaCepResponse response) {
+        try {
+            String json = objectMapper.writeValueAsString(response);
+
+            CepLog log = CepLog.builder()
+                    .cep(cep)
+                    .response(json)
+                    .dataConsulta(LocalDateTime.now())
+                    .build();
+
+            repository.save(log);
+
+        } catch (Exception e) {
+            // evita quebrar a aplicação por causa de log
+            System.out.println("Erro ao salvar log: " + e.getMessage());
+        }
     }
 }
-
-
